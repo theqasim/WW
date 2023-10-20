55e99305635aeb1505948bbd11ab4fca516eb18a
theqasim <kasim7862k@gmail.com>
2023-10-20 17:58:49 +0100

    added protection for new lesson

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/lessons/dropshipwithnomoney", "/lessons/makemoneywith$0"],
+  matcher: ["/lessons/dropshipwithnomoney", "/lessons/makemoneywith$0", "/lessons/howtomakepassiveincomewithchatgpt"],
 };
