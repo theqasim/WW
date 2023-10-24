3c5623adc93a809180b029e5b1b65b64c8840199
theqasim <kasim7862k@gmail.com>
2023-10-24 22:41:15 +0100

    middleware update

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/lessons/dropshipwithnomoney", "/lessons/makemoneywith$0", "/lessons/howtomakepassiveincomewithchatgpt"],
+  matcher: ["/lessons/dropshipwithnomoney", "/lessons/makemoneywith$0", "/lessons/makemoneywithchatgpt"],
 };
