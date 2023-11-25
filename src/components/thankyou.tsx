d8eace0d1b7da888b920dc3e25a9d7e1e6941dfe
theqasim <kasim7862k@gmail.com>
2023-11-25 23:04:24 +0000

    fix

--- src/components/thankyou.tsx
+++ src/components/thankyou.tsx
@@ -1,36 +1,22 @@
 "use client";
 
 import React, { useEffect, useRef } from "react";
-import Plyr from "plyr";
-import "plyr/dist/plyr.css";
 
 function Thankyou() {
-  const videoRef = useRef(null);
-
-  useEffect(() => {
-    if (videoRef.current) {
-      const player = new Plyr(videoRef.current);
-
-      return () => {
-        player.destroy();
-      };
-    }
-  }, []);
-
   return (
     <div className="flex flex-col items-center justify-center md:w-3/4 w-4/4">
       <h2 className="text-3xl mt-4 font-bold text-center mb-6">
         Thank you for joining, your exclusive invite is below, along with a
         video from me to you 👇
       </h2>
-      <video ref={videoRef} controls className="w-full">
+      {/* <video ref={videoRef} controls className="w-full">
         <source
           src="https://www.dropbox.com/scl/fi/zhz5f16g19vqekhwuxgcg/WW-Join-Video.mp4?rlkey=asw9hv56k85rq7zs27sjkk9pw&dl=1"
           type="video/mp4"
           className=" "
         />
         Your browser does not support the video tag.
-      </video>
+      </video> */}
       <a
         href="https://discord.gg/ENHePPhAF"
         target="_blank"
