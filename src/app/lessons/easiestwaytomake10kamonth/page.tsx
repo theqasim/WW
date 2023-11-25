e8ea97e2cb94ae047e1e5712c7ef6c180231d105
theqasim <kasim7862k@gmail.com>
2023-11-25 22:48:39 +0000

    update

--- src/app/lessons/easiestwaytomake10kamonth/page.tsx
+++ src/app/lessons/easiestwaytomake10kamonth/page.tsx
@@ -3,22 +3,23 @@
 import React from "react";
 import Link from "next/link";
 import Plyr from "plyr";
+import YouTube from "react-youtube";
 import { useEffect, useRef } from "react";
 import "plyr/dist/plyr.css";
 import Backtolessons from "@/components/backtolessons";
 
 const VideoBlog: React.FC = () => {
-  const videoRef = useRef(null);
+  const opts = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
 
-  useEffect(() => {
-    if (videoRef.current) {
-      const player = new Plyr(videoRef.current);
-
-      return () => {
-        player.destroy();
-      };
-    }
-  }, []);
+  const onReady = (event: { target: { pauseVideo: () => void } }) => {
+    event.target.pauseVideo();
+  };
 
   return (
     <div className="min-h-screen bg-gray-200 flex items-center justify-center">
@@ -26,13 +27,7 @@ const VideoBlog: React.FC = () => {
         <h1 className="text-2xl font-bold mb-4 text-center text-gray-800">
           The Easiest Way to make $10k a month (TikTok Creativity Program){" "}
         </h1>
-        <video ref={videoRef} controls className="w-full mb-6">
-          <source
-            src="https://www.dropbox.com/scl/fi/rju41l1rmcd68aqundh2s/How-I-would-start-Dropshipping-with-0.mp4?rlkey=7ssy5ez50kh6mjnlpsx2rbhfq&dl=1"
-            type="video/mp4"
-          />
-          Your browser does not support the video tag.
-        </video>
+        <YouTube videoId="Xv1Kub4iZt0" opts={opts} onReady={onReady} />
 
         <h2 className="text-xl font-bold text-center">
           There&apos;s always money making opportunities that you wish you were
